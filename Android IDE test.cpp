98823adaac39566a4c5acb3aeb41f97651c40d60
Ryan Ye <yulinye.yy@gmail.com>
2013-09-09 16:34:38 -0700

    fork

--- Android IDE test.cpp
+++ Android IDE test.cpp
@@ -1,20 +1,30 @@
-#include <iostream>
+/*
+/ This program fork a child process while the parent process does not wait. 
+/The result should be that tthere is a defunc process shown in the result of 
+/ps command.
+*/
+
 #include <iomanip>
+#include <stdio.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
+   signal(SIGCHLD,SIG_IGN);
+   PID_t PID;
+   
+   PID=fork();
+   
+   if (PID==0)
+   {
+      execlp("/bin/ls","ls",NULL);
+      cout<<"this is the child process!";
+   }else if(PID>0)
+   {
+      cout<<"this is the parent process!"
+   }
 
-cout<<"hello, world!"<<endl;
 
-cout<<"changed a little more!"<<endl;
-
-cout<<"check it again!";
-
-count<<"changed a little!";
-
-return 0; 
+   return 0;
 }
-
-
-
